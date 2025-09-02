d3dc45399c7488a4819ef3b1e9051906cf7ae822
prathyusha siri <Manohar>
2025-09-02 14:17:33 +0530

    Your commit message here

--- shadcn-app/src/app/layout.tsx
+++ shadcn-app/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} flex items-start justify-between antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} flex items-start justify-start  antialiased`}
       >
         <Sidebar />
         <main className="grid h-full">
--- shadcn-app/src/app/page.tsx
+++ shadcn-app/src/app/page.tsx
@@ -1,7 +1,19 @@
 import General from "@/components/Cards/General";
+import { Card } from "@/components/ui/card";
 
 export default function page() {
-  return <div className="grid grid-cols-2">
+  return <div className="grid gap-[32px]">
+    <div className="grid grid-cols-2 gap-[32px]">
    <General />
+   <div className="grid gap-[32px]">
+      <Card />
+      <Card />
+   </div>
+   <div className="grid grid-cols-3 gap-[32px]">
+        <Card className="h-[300px]">Hello</Card>
+        <Card className="h-[300px]">Manohar</Card>
+        <Card className="h-[300px]">Sai</Card>
+   </div>
     </div>
+  </div>
 } 
